91569f6b165eae7425d9368044a11b08f8e1fb69
ShanthVerse <shanth24kumar@gmail.com>
2022-12-14 11:51:55 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "JSON name/value pair is written as",
+        "opt1" : "'name’ : ‘value’",
+        "opt2" : "name = ‘value’",
+        "opt3" : "“name” : “value”",
+        "answer" : "“name” : “value”"
+    },
+    {
+        "q" : "Which of the following is not a JSON type?",
+        "opt1" : "Object",
+        "opt2" : "Date",
+        "opt3" : "String",
+        "answer" : "Date"
+    },
+    {
+        "q" : "In modern websites what is the common usage for JSON?",
+        "opt1" : "To store information remotely.",
+        "opt2" : "To send and receive bits of data.",
+        "opt3" : "To store information locally.",
+        "answer" : "To send and receive bits of data."
+    },
+    {
+        "q" : "What keywords are reserved in JSON and cannot be used as keys?",
+        "opt1" : "Value",
+        "opt2" : "Object",
+        "opt3" : "There are none.",
+        "answer" : "There are none."
+    },
+    {
+        "q" : "What function will convert a JavaScript object to a JSON string?",
+        "opt1" : "JSON.text()",
+        "opt2" : "JSON.toString()",
+        "opt3" : "JSON.stringify()",
+        "answer" : "JSON.stringify()"
+    }
+];
